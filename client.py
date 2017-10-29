7587b2031a388bb012e6f1c000e4270892e26d1d
monicagomezavila <monicagomezavila@github.com>
2017-10-29 10:44:23 +0100

    Modifica el mensaje mandado por el cliente si está de la forma: register dirección

--- client.py
+++ client.py
@@ -7,15 +7,25 @@ Programa cliente UDP que abre un socket a un servidor
 import sys
 import socket
 
-# Constantes. Dirección IP del servidor y contenido a enviar
+
+# Constantes. Dirección IP del servidor y contenido a enviar.
 SERVER = str(sys.argv[1])
 PORT = int(sys.argv[2])
 z = (sys.argv[3:])
+print(z)
 
 LINE = ''
-for elemento in z:
-    LINE += elemento + ' '
-LINE = LINE[:LINE.rfind(' ')]
+if sys.argv[3] == 'register':
+    LINE += 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
+
+elif sys.argv[3] == 'REGISTER':
+    for elemento in z:
+        LINE += elemento + ' '
+    LINE = LINE[:LINE.rfind(' ')]
+    LINE = (LINE[:LINE.rfind('0')+1] + "\r\n\r\n")
+
+else:
+    sys.exit('El mensaje sip está mal formulado')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
