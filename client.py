75d62af8dfe962cadc2f667dd1b598ee3a96a539
monicagomezavila <monicagomezavila@github.com>
2017-10-29 12:59:51 +0100

    Pasa los datos de la shell a un mensaje SIP

--- client.py
+++ client.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente UDP que abre un socket a un servidor
+Programa cliente UDP que abre un socket a un servidor y manda un mensaje SIP
 """
 
 import sys
 import socket
 
+if len(sys.argv) != 6:
+    print('Usage: client.py ip puerto register sip_address expires_value')
+    sys.exit()
 
-# Constantes. Dirección IP del servidor y contenido a enviar.
+# Dirección IP del servidor y contenido a enviar.
 SERVER = str(sys.argv[1])
 PORT = int(sys.argv[2])
 z = (sys.argv[3:])
-print(z)
 
 LINE = ''
 if sys.argv[3] == 'register':
-    LINE += 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
-
-elif sys.argv[3] == 'REGISTER':
+    LINE += ('REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n')
+    LINE += ('Expires: ' + sys.argv[5] + '\r\n\r\n')
+else:
     for elemento in z:
         LINE += elemento + ' '
-    LINE = LINE[:LINE.rfind(' ')]
-    LINE = (LINE[:LINE.rfind('0')+1] + "\r\n\r\n")
+        LINE = LINE[:LINE.rfind(' ')]
 
-else:
-    sys.exit('El mensaje sip está mal formulado')
+if int(sys.argv[5]) < 0:
+    sys.exit('El tiempo debe ser >= 0')
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
