dafcb91cfc51c0dddc19c11403f7cc04829fa05f
Monica Gomez Avila <mgavila@gamma03.aulas.gsyc.urjc.es>
2017-10-30 15:58:11 +0100

    Eliminado lineas de más

--- client.py
+++ client.py
@@ -31,9 +31,8 @@ if int(sys.argv[5]) < 0:
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print(data.decode('utf-8'))
 
 print("Socket terminado.")
