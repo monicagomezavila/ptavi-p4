05fb7d3d5f0a0cf969dd2e900432305891fb2d87
Monica Gomez Avila <mgavila@alpha30.aulas.gsyc.urjc.es>
2017-10-31 15:35:38 +0100

    Programa final

--- client.py
+++ client.py
@@ -11,7 +11,9 @@ if len(sys.argv) != 6:
     print('Usage: client.py ip puerto register sip_address expires_value')
     sys.exit()
 
-# Dirección IP del servidor y contenido a enviar.
+"""
+Dirección IP del servidor y contenido a enviar.
+"""
 SERVER = str(sys.argv[1])
 PORT = int(sys.argv[2])
 z = (sys.argv[3:])
@@ -28,7 +30,9 @@ else:
 if int(sys.argv[5]) < 0:
     sys.exit('El tiempo debe ser >= 0')
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
+"""
+Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
+"""
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
